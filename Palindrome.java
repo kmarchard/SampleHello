aeb58d2b058208befbd7af435f913e390bef6378
kmarchard <44217438+kmarchard@users.noreply.github.com>
2019-02-05 13:09:04 +0530

    Hard coded the variable

--- Palindrome.java
+++ Palindrome.java
@@ -5,13 +5,7 @@ public class Palindrome
    public static void main(String args[])
    {
       String original, reverse = "MADAM"; // Objects of String class
-      
-     
-      int length = original.length();
-     
-      for (int i = length - 1; i >= 0; i--)
-         reverse = reverse + original.charAt(i);
-         
+
       if (original.equals(reverse))
          System.out.println("The string is a palindrome.");
       else
