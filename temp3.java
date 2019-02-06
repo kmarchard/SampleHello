dc8b3345aea971c7af686ce8509e35251676d8f7
kmarchard <krish6898@gmail.com>
2019-02-06 15:48:55 +0530

    updated the print message for jenkin 1234

--- temp3.java
+++ temp3.java
@@ -2,6 +2,6 @@ public class temp3{
    public static void main(String[] args) {
          // Prints "Hello, World" in the terminal window.
 	       System.out.println("Welcome Git and Polling SCM");
-	       System.out.println("Welcome to world of Git Hub and jenkin 123"); 
+	       System.out.println("Welcome to world of Git Hub and jenkin 1234444"); 
 	          }
 		  }
