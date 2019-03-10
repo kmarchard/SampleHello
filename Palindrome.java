ded7a0b3466d8578524500f90aded5a9982c390a
kmarchard <44217438+kmarchard@users.noreply.github.com>
2019-03-10 13:31:06 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.util.*;
 public class Palindrome
 {
    public static void main(String args[])
    {
       Scanner sc = new Scanner(System.in); 
-      int age = sc.nextInt();
-      System.out.println("The age is"+ age);
+      //int age = sc.nextInt();
+      System.out.println("The age is"+  12);
+      String name=”quora”;
+
+StringBuilder strb=new StringBuilder(name);
+
+System.out.println("Checking import"+" "+strb.reverse().toString());
       String original= "MADAM"; // Objects of String class
 String reverse= "MADAM";
       if (original.equals(reverse))
